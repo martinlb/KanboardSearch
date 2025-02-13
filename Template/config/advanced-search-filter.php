eeb042c7f612548ba64ca747cae79536ae85492e
Martin Le Blévennec <martinlb@free.fr>
2025-02-13 22:25:34 +0100

    Update advanced-search-filter.php
    PHP 8.4 compatibility

--- Template/config/advanced-search-filter.php
+++ Template/config/advanced-search-filter.php
@@ -5,24 +5,24 @@
     <?= $this->form->csrf() ?>
 
     <fieldset>
-        <?= $this->form->checkbox('comment_search', t('Enable "Search in Comment"'), 1, $values['comment_search'] == 1) ?>
+        <?= $this->form->checkbox('comment_search', t('Enable "Search in Comment"'), 1, isset($values['comment_search']) && $values['comment_search'] == 1) ?>
     </fieldset>
     <fieldset>
-        <?= $this->form->checkbox('title_search', t('Enable "Search in Title"'), 1, $values['title_search'] == 1) ?>
-    </fieldset>
-
-    <fieldset>
-        <?= $this->form->checkbox('description_search', t('Enable "Search in Description"'), 1, $values['description_search'] == 1) ?>
+        <?= $this->form->checkbox('title_search', t('Enable "Search in Title"'), 1, isset($values['title_search']) && $values['title_search'] == 1) ?>
     </fieldset>
 
     <fieldset>
-        <?= $this->form->checkbox('subtask_search', t('Enable "Search in Subtask Title"'), 1, $values['subtask_search'] == 1) ?>
+        <?= $this->form->checkbox('description_search', t('Enable "Search in Description"'), 1, isset($values['description_search']) && $values['description_search'] == 1) ?>
+    </fieldset>
+
+    <fieldset>
+        <?= $this->form->checkbox('subtask_search', t('Enable "Search in Subtask Title"'), 1, isset($values['subtask_search']) && $values['subtask_search'] == 1) ?>
     </fieldset>
     <fieldset>
-        <?= $this->form->checkbox('attachment_search', t('Enable "Search in Attachments"'), 1, $values['attachment_search'] == 1) ?>
+        <?= $this->form->checkbox('attachment_search', t('Enable "Search in Attachments"'), 1, isset($values['attachment_search']) && $values['attachment_search'] == 1) ?>
     </fieldset>
     <fieldset>
-        <?= $this->form->checkbox('id_search', t('Enable "Search in Ids"'), 1, $values['id_search'] == 1) ?>
+        <?= $this->form->checkbox('id_search', t('Enable "Search in Ids"'), 1, isset($values['id_search']) && $values['id_search'] == 1) ?>
     </fieldset>
 
     <?= $this->hook->render('template:config:advanced-search-filter', array('values' => $values, 'errors' => $errors)) ?>
